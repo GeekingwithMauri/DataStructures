47b1aa8334265290a1a1794659e23b01e4a9d513
Mauricio Chirino <1657723+mchirino89@users.noreply.github.com>
2023-01-04 11:53:41 -0300

    A bit of doc

--- Sources/DataStructures/DataStructurable.swift
+++ Sources/DataStructures/DataStructurable.swift
@@ -7,10 +7,17 @@
 
 import Foundation
 
+/// Set of properties and methods all data structures should share
 public protocol DataStructurable: AnyObject {
     associatedtype Element
 
+    /// Number of elements within the data structure at all times
+    var count: Int { get }
+
+    /// Whether the data structure is empty (no elements)
     var isEmpty: Bool { get }
 
+    /// The top most element for a given data structure.
+    /// - Returns: the value of the `Element` type the data structure is holding
     func peek() -> Element?
 }
