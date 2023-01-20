ff7ce4e0893926721e193502c8c8175196ddfae5
Mauricio Chirino <1657723+mchirino89@users.noreply.github.com>
2023-01-20 10:30:42 -0300

    Update doc

--- Sources/DataStructures/DataStructures/Non-linear/BinaryTree.swift
+++ Sources/DataStructures/DataStructures/Non-linear/BinaryTree.swift
@@ -7,22 +7,39 @@
 
 import Foundation
 
+/// Binary tree implementation for any type of numeric value
 public final class BinaryTree<T: Numeric & Comparable> {
+    /// Nodes count
+    var count: Int
+
+    /// Tree's root node
     var root: TreeNode<T>?
 
-    public func insert(_ value: T) {
-        let newNode = TreeNode(value)
+    /// Default initialization
+    public init() {
+        count = 0
+    }
+
+    /// Inserts values in a left-most orderly fashion
+    /// - Parameter newValue: numeric value to be added
+    public func insert(_ newValue: T) {
+        let newNode = TreeNode(newValue)
+
         if root == nil {
             root = newNode
+            count += 1
+
             return
         }
 
         var current = root
 
         while true {
-            if value < current!.value {
+            if newValue < current!.value {
                 if current?.left == nil {
                     current?.left = newNode
+                    count += 1
+
                     break
                 } else {
                     current = current?.left
@@ -30,11 +47,43 @@ public final class BinaryTree<T: Numeric & Comparable> {
             } else {
                 if current?.right == nil {
                     current?.right = newNode
+                    count += 1
+
                     break
                 } else {
                     current = current?.right
                 }
             }
+        }
+    }
+
+    /// Prints value in left-current-right order
+    /// - Parameter node: starting point node
+    public func inOrderTraversal(node: TreeNode<T>?) {
+        if let currentNode = node {
+            inOrderTraversal(node: currentNode.left)
+            print(currentNode.value)
+            inOrderTraversal(node: currentNode.right)
+        }
+    }
+
+    /// Prints value in current-left-right order
+    /// - Parameter node: starting point node
+    public func preOrderTraversal(node: TreeNode<T>?) {
+        if let currentNode = node {
+            print(currentNode.value)
+            preOrderTraversal(node: currentNode.left)
+            preOrderTraversal(node: currentNode.right)
+        }
+    }
+
+    /// Prints value in right-current-left order
+    /// - Parameter node: starting point node
+    public func postOrderTraversal(node: TreeNode<T>?) {
+        if let currentNode = node {
+            postOrderTraversal(node: currentNode.right)
+            print(currentNode.value)
+            postOrderTraversal(node: currentNode.left)
         }
     }
 }
---
+++ Tests/DataStructuresTests/Non-linear/BinaryTreeTests.swift
@@ -0,0 +1,29 @@
+@testable import DataStructures
+import XCTest
+
+final class BinaryTreeTests: XCTestCase {
+    func preloadedTree() -> BinaryTree<Int> {
+        let stubbedValues = [20, 23, 37, 39, 53, 54, 60, 76, 83, 100]
+        let sut = BinaryTree<Int>()
+
+        for i in stubbedValues {
+            sut.insert(i)
+        }
+
+        return sut
+    }
+
+    func test_guaranteeAllValuesAreCounted() {
+        // Given
+        let definedCount = 10
+        let sut = BinaryTree<Int>()
+
+        // When
+        for i in 1...definedCount {
+            sut.insert(i)
+        }
+
+        // Verify
+        XCTAssertEqual(sut.count, definedCount, "Internal binary tree count isn't working as expected")
+    }
+}
